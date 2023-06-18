10195d68966d8b12d08ea8f643be514c1844780d
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-06-18 09:03:24 +0300

    update cv name

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/CV.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Here's my CV</a>
+        <a href={CV} download="KhangNguyen-CV" className='btn'>Here's my CV</a>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
