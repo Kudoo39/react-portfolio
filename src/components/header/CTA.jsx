40dbb2307d310e579c45b5feb60375c7c3b78781
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-12 19:45:16 +0300

    customizing UX

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import CV from '../../assets/CV.pdf'
+import React from "react";
 
 const CTA = () => {
   return (
-    <div className='cta'>
-        <a href={CV} download="KhangNguyen-CV" className='btn'>Here's my CV</a>
-        <a href="#contact" className='btn btn-primary'>Contact Me</a>
+    <div className="cta">
+      <a
+        target="_blank"
+        href="https://drive.google.com/file/d/19y1FYeWjaQRnfpOBVy7PO26yTEbesIg-/view?usp=sharing"
+        className="btn"
+      >
+        Here's my CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Contact Me
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
