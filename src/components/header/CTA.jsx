b3e6f471ad6ad68b589cd96c1bb0761b03de4b7a
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2025-04-30 22:51:09 +0300

    update CV

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <div className="cta">
+    <div className='cta'>
       <a
-        target="_blank"
-        href="https://drive.google.com/file/d/1VMkK2Ita4d_mqHOjzHw-yV9J8fOfVTRF/view?usp=sharing"
-        className="btn"
-        rel="noreferrer"
+        target='_blank'
+        href='https://drive.google.com/file/d/1ZYplo_ujHitg2U0k8p09mfHgkwpQL5bB/view?usp=sharing'
+        className='btn'
+        rel='noreferrer'
       >
         Here's my CV
       </a>
-      <a href="#contact" className="btn btn-primary">
+      <a href='#contact' className='btn btn-primary'>
         Contact Me
       </a>
     </div>
